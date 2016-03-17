73f216158df5add5b7710cf4a25846c47b5f915b
Final_Lap <547805712@qq.com>
2016-03-17 23:29:00 +0800

    config修改

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'checkclass.aifuwu.org';
+$config['base_url'] = 'http://checkclass.aifuwu.org';
 
 /*
 |--------------------------------------------------------------------------
