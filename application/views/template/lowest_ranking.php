4109e84f76ce20c41d11f5be9bac705f876169b1
Final_Lap <547805712@qq.com>
2016-03-30 12:35:32 +0800

    增加表格换行逻辑

--- application/views/template/lowest_ranking.php
+++ application/views/template/lowest_ranking.php
@@ -5,6 +5,40 @@
                   <p>今日已录入<?php echo $today_data_count;?>节课查课信息！</p>
 
                     <?php
+                      /**
+                       * 指定位置插入字符串
+                       * @param $str  原字符串
+                       * @param $i    插入位置
+                       * @param $substr 插入字符串
+                       * @return string 处理后的字符串
+                       */
+                      function insertToStr($str, $i, $substr)
+                      {
+                        //指定插入位置前的字符串
+                        $startstr="";
+                        for($j=0; $j<$i; $j++)
+                        {
+                            $startstr .= $str[$j];
+                        }
+                          
+                        //指定插入位置后的字符串
+                        $laststr="";
+                        for ($j=$i; $j<strlen($str); $j++){
+                            $laststr .= $str[$j];
+                        }
+                          
+                        //将插入位置前，要插入的，插入位置后三个字符串拼接起来
+                        $str = $startstr . $substr . $laststr;
+                          
+                        //返回结果
+                        return $str;
+                      }
+
+                      function class_list_process($class_list)
+                      {
+                        return insertToStr($class_list, 20, "\n");
+                      }
+
                       if($course_list)
                       {
                         $table_header = <<<EOF
@@ -30,7 +64,7 @@ EOF;
                           echo '<td width="60"><p align="center">'.$rank.'</p></td>';
                           echo '<td width="230"><p align="center">'.$course_detail['course_name'].'</p></td>'."\n";
                           echo '<td width="100"><p align="center">'.'第'.$course_detail['class_time'].'大节</p></td>'."\n";
-                          echo '<td width="140"><p align="center">'.$course_detail['class_list'].'</p></td>'."\n";
+                          echo '<td width="140"><p align="center">'.class_list_process($course_detail['class_list']).'</p></td>'."\n";
                           echo '<td width="60"><p align="center">'.$course_detail['choices_number'].'</p></td>'."\n";
                           echo '<td width="60"><p align="center">'.$course_detail['real_number_min'].'</p></td>'."\n";
                           echo '<td width="80"><p align="center">'.$course_detail['class_rate_min'].'</p></td>'."\n";
